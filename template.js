bf4dafa56254d2c69648053481cd88b19340025d
samreed18 <79289946+samreed18@users.noreply.github.com>
2021-04-19 11:04:39 +0100

    Update template.js

--- template.js
+++ template.js
@@ -2,9 +2,11 @@ const urlParams = new URLSearchParams(queryString);
 
 const star = urlParams.get('star')
 
+console.log("run")
 fetch("/star.json")
 .then((response) => {
     return response.json()
+    console.log("run")
 })
 .then((data) => {
   console.log(data)
